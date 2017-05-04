41039c8453fcf270da18dd529e62689c7c04642f
Ninja <jiachenning@gmail.com>
2017-05-04 15:12:34 +0800

    Remove unuse dir

--- app/src/main/java/com/zeonic/icity/location_picker/MainActivity.java
+++ app/src/main/java/com/zeonic/icity/location_picker/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // TODO: 5/4/17  
+        // TODO: 5/4/17
         LocationClient mLocationClient = new LocationClient(getApplicationContext());
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
